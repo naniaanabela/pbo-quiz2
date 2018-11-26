4a1619a7357c891d42c3569f0a74f4ae2df23f7a
Nurcahya Nania A <nania.anabela22@gmail.com>
2018-11-26 18:48:27 +0700

    menambahkan HashMap pada class TranComboModel

---
+++ src/com/NurcahyaNaniaAnabela/Library/TranComboModel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.NurcahyaNaniaAnabela.Library;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author NurcahyaNaniaA
+ */
+public class TranComboModel {
+    private HashMap<String, Float> items = new HashMap<>();
+}
