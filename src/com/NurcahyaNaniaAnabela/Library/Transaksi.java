a969cc504c8c9c091547dd3a078aeb41dc9643d1
Nurcahya Nania A <nania.anabela22@gmail.com>
2018-11-26 18:40:57 +0700

    menambahkan setter total pada class Transaksi

--- src/com/NurcahyaNaniaAnabela/Library/Transaksi.java
+++ src/com/NurcahyaNaniaAnabela/Library/Transaksi.java
@@ -21,4 +21,12 @@ public class Transaksi {
         this.code = code;
         this.total = total;
     }
+
+    public void setTotal() {
+        float total = 0;
+        for(Item item : this.items){
+            total += item.getTotal();
+        }
+        this.total = total;
+    }
 }
