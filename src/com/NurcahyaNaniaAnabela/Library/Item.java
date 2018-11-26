7eb0982aee0d352832f5a7cb4102d40590149358
Nurcahya Nania A <nania.anabela22@gmail.com>
2018-11-26 18:19:07 +0700

    menambahkan atribut pada class Item

---
+++ src/com/NurcahyaNaniaAnabela/Library/Item.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.NurcahyaNaniaAnabela.Library;
+
+/**
+ *
+ * @author NurcahyaNaniaA
+ */
+public class Item {
+    private String nama; // variable nama untuk menyimpan nama item
+    private float harga; // variable harga untuk menyimpan item harga
+    private int jumlah; // jumlah untuk menyimpan item jumlah
+
+
+    
+    
+}
