e4bf853122210f7878319f27682f0bba7d101cea
Nurcahya Nania A <nania.anabela22@gmail.com>
2018-11-27 12:30:21 +0700

    menambahkan variable array yang bertipe String untuk menyimpan kolom nama

---
+++ src/com/NurcahyaNaniaAnabela/Library/TransaksiTabel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.NurcahyaNaniaAnabela.Library;
+
+/**
+ *
+ * @author NurcahyaNaniaA
+ */
+public class TransaksiTabel {
+    private String[] kolom; //Variable Array bertipe string untuk menyimpan kolom nama 
+}
