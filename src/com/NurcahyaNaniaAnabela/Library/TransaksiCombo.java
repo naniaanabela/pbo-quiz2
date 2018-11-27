f922d133cc9095dd4b0ec5dd67d69ba394fe8cb8
Nurcahya Nania A <nania.anabela22@gmail.com>
2018-11-27 12:19:35 +0700

    menambahkan method ArrayList memanggil item

--- src/com/NurcahyaNaniaAnabela/Library/TransaksiCombo.java
+++ src/com/NurcahyaNaniaAnabela/Library/TransaksiCombo.java
@@ -5,6 +5,7 @@
  */
 package com.NurcahyaNaniaAnabela.Library;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -14,9 +15,18 @@ import java.util.HashMap;
 public class TransaksiCombo {
     private HashMap<String , Float> items = new HashMap<>();
 
+    //Konstruktor class TransaksiCombo
     public TransaksiCombo() {
         this.items.put("Gula", new Float(30000));
         this.items.put("Kopi", new Float(10000));
         this.items.put("Susu", new Float(20000));
+    }
+    
+    public ArrayList<String> getNama(){
+        ArrayList<String> string = new ArrayList<>();
+        for(String item : this.items.keySet()){
+            string.add(item);
+        }
+        return string;
     }
 }
